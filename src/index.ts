73418e7d760a0e4ddb497bfc0340203b52e439d3
Divyansh Sharma <divyanshsharma4507@gmail.com>
2025-01-23 21:23:20 +0530

    Add WebSocket server implementation and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello, world!');
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+//event handler
+
+wss.on('connection', function(socket){
+    console.log('user connected')   
+    setInterval(()=>{
+     socket.send("Current price of bitcoin is " + Math.random());
+    }, 500)
+     
+    // this is used to listen to the message event from the client
+    socket.on('message', (e)=>{
+        console.log(e.toString());
+    })
+
+})
