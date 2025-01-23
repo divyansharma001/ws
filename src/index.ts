bff96d66d4cbb70290de45b0a484ff7c315fd27e
Divyansh Sharma <divyanshsharma4507@gmail.com>
2025-01-23 21:30:56 +0530

    Add ping-pong response to WebSocket messages

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,16 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', function(socket){
     console.log('user connected')   
-    setInterval(()=>{
-     socket.send("Current price of bitcoin is " + Math.random());
-    }, 500)
-     
+   
     // this is used to listen to the message event from the client
     socket.on('message', (e)=>{
-        console.log(e.toString());
+
+        console.log(e.toString())
+        console.log(e.toString().trim()=='ping')
+
+        if(e.toString().trim()=='ping'){
+            socket.send('pong')
+        }
     })
 
 })
