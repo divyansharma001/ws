508b140af0792f47eea4701ac23190eec212781f
Divyansh Sharma <divyanshsharma4507@gmail.com>
2025-01-24 19:54:12 +0530

    Implement WebSocket client in App component to send and receive messages

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [socket, setWebsocket] = useState();
+
+  
+
+  const handleSendMessage = () =>{
+    if(!socket) return;
+    //@ts-ignore
+    socket.send("ping");
+  }
+
+  useEffect(()=>{
+    const ws = new WebSocket('ws://localhost:8080')
+    setWebsocket(ws)
+    ws.onmessage = (message) => {
+      console.log('Message received:', message.data)
+    }
+   }, [])
+
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <input type='text' placeholder='Write Message'>
+      </input>
+      <button onClick={handleSendMessage}>Send</button>
+    </div>
+      
     </>
   )
 }
